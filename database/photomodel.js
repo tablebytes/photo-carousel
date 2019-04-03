40b13c5ca35f9bd1f32daa1eb75fb2bfa7f9cfec
Pha-Le Nguyen <phalenguyen15@gmail.com>
2019-04-03 16:38:42 -0700

    update ID variable for restaurant and database model.

--- database/photomodel.js
+++ database/photomodel.js
@@ -1,19 +1,13 @@
 const mongoose = require('mongoose');
-const db = require('./index');
-
-mongoose.Promise = global.Promise;
 
 const photoSchema = mongoose.Schema({
-  id: Number,
-  image: {
+  restaurantId: Number,
+  image: [{
     imageUrl: String,
     comment: String,
     date: Date,
-  },
+  }],
 });
-const getPhotos = () => {
-
-}
 
 const Photo = mongoose.model('Photo', photoSchema);
 
