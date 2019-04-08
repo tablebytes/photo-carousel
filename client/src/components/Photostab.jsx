d79b013aba57950101f9c832ef73e372fb42623d
Pha-Le Nguyen <phalenguyen15@gmail.com>
2019-04-08 15:03:53 -0700

    update variable names for Photostab.

--- client/src/components/Photostab.jsx
+++ client/src/components/Photostab.jsx
@@ -5,18 +5,17 @@ class Photostab extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      modal: 'none'
     }
   }
 
   render() {
     return (
       this.props.photos && 
-      <div className="photoGridContainer">
+      <div className="photoGridMainContainer">
         <div className="row">
         {this.props.photos.map((photo, idx) => 
           <div key={idx} onClick={() => this.props.showModalState(true, idx)} >
-           <div className="photoContainer">
+           <div className="photoGridContainer">
             <div className="column">
             <img className="photoGridPhoto" src={photo.imageUrl} alt={photo.comment}/>
             </div>
