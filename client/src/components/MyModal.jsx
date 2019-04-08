be3886ad3b6c3a11addb2bd38f6b87e427de2422
Pha-Le Nguyen <phalenguyen15@gmail.com>
2019-04-08 01:03:02 -0700

    add styling for photo grid component and modal.

--- client/src/components/MyModal.jsx
+++ client/src/components/MyModal.jsx
@@ -20,8 +20,8 @@ class MyModal extends React.Component {
     <div id="modal" className="modal">
       <span className="close cursor"  onClick={() => this.props.showModalState(false)}>&times;</span>
       <div className="modal-content">
-        <div>
-          <img width='100%'src={this.props.photos[this.state.slideIndex].imageUrl}></img>
+        <div >
+          <img className="modalPhoto" src={this.props.photos[this.state.slideIndex].imageUrl}></img>
         </div>
       <a className="prev" onClick={this.plusSlides.bind(this, -1)}>&#10094;</a>
       <a className="next" onClick={this.plusSlides.bind(this, 1)}>&#10095;</a>
--- client/src/components/Photostab.jsx
+++ client/src/components/Photostab.jsx
@@ -12,22 +12,19 @@ class Photostab extends React.Component {
   render() {
     return (
       this.props.photos && 
-      <div>
+      <div >
         <div className="row">
         {this.props.photos.map((photo, idx) => 
-          <div key={idx} className={`p${idx.toString()}`} onClick={() => this.props.showModalState(true, idx)} >
-           <div>
-          <a className="photos-gallery-link__2P8j-ADA" role="button">
-          <div className="column">
-          <img className={`p${idx.toString()}`} width='100%' src={photo.imageUrl} alt={photo.comment}/>
+          <div key={idx} onClick={() => this.props.showModalState(true, idx)} >
+           <div className="photoContainer">
+            <div className="column">
+            <img className="photoC" src={photo.imageUrl} alt={photo.comment}/>
+            </div>
           </div>
-          </a>
-          </div>
-          </div>
+        </div>
         )}
         </div>
       </div>
-
     );
   }
 }
