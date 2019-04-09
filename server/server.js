087667e79ef3c2c5ef8cf6570442079168ae257c
Pha-Le Nguyen <phalenguyen15@gmail.com>
2019-04-09 16:39:55 -0700

    add cors.

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const path = require('path');
 const { Photo } = require('../database/photomodel');
 const { db } = require('../database/index');
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
